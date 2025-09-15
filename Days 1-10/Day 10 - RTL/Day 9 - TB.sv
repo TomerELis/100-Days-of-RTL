fc072d9a1205af0d6e163bea9bd141cf1a114304
TomerELis <121366350+TomerELis@users.noreply.github.com>
2025-09-16 00:51:17 +0300

    Delete Days 1-10/Day 10 - RTL directory

--- Days 1-10/Day 10 - RTL/Day 9 - TB.sv
+++
@@ -1,9 +0,0 @@
-// ============================================
-// Module: day9 TB
-// Purpose: Counter with a load
-// Date: September 12, 2025
-// Tomer Elis
-
-//Explanation: counter which supports loading a value and restarting from the last loaded value.
-//Example: 
-// ============================================
